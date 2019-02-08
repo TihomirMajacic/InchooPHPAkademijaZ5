fde6c3aa61303e4c3e06fddc4351b8a53e3182e8
TihomirMajacic <majacictihomir@gmail.com>
2019-02-08 15:29:18 +0100

    Overwritten config.php

---
+++ app/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'url' => 'http://polaznik20mvc.inchoo4u.net/',
+    'mode'  => 'development',
+    "db"=>[
+        "host"=>"localhost",
+        "name"=>"social_network",
+        "user"=>"root",
+        "password"=>""
+    ]
+];
